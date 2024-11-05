950f1517442144abfb5c105bf835fcc3c4a50b82
aldotestino <aldotestino4@gmail.com>
2024-11-05 15:32:38 +0100

    create and get posts

--- src/db/middleware.ts
+++ src/db/middleware.ts
@@ -1,10 +1,10 @@
 import type { MiddlewareHandler } from 'hono';
-import { Pool } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-serverless';
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
 
 export const dbMiddleware: MiddlewareHandler = async (c, next) => {
-  const client = new Pool({ connectionString: c.env.DATABASE_URL });
-  const db = drizzle(client);
+  const sql = neon(c.env.DATABASE_URL);
+  const db = drizzle({ client: sql });
   c.set('db', db);
 
   await next();
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,39 @@
+import type { NeonQueryFunction } from '@neondatabase/serverless';
+import type { NeonHttpDatabase } from 'drizzle-orm/neon-http';
 import { Hono } from 'hono';
 import { dbMiddleware } from './db/middleware';
+import { posts } from './db/schema';
 
 type Env = {
   DATABASE_URL: string;
 };
 
-const app = new Hono<{ Bindings: Env }>();
+type Variables = {
+  db: NeonHttpDatabase<Record<string, never>> & {
+    $client: NeonQueryFunction<false, false>;
+  };
+};
+
+const app = new Hono<{ Bindings: Env; Variables: Variables }>();
 
 app.use(dbMiddleware);
 
-app.get('/', (c) => {
-  return c.json({
-    message: 'Deployed by Github Action',
-  });
+app.get('/', async (c) => {
+  const db = c.get('db');
+
+  const allPosts = await db.select().from(posts);
+
+  return c.json(allPosts);
+});
+
+app.post('/', async (c) => {
+  const db = c.get('db');
+
+  const { title, content } = await c.req.json();
+
+  const newPost = await db.insert(posts).values({ title, content }).returning();
+
+  return c.json(newPost);
 });
 
 export default app;
